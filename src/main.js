4ce2fe809b4cfdc7a77e6e42d7609d06b4fa2994
Alexander <alxndr+github@gmail.com>
2016-11-06 14:32:25 -0800

    polyfill for fetch

--- src/main.js
+++ src/main.js
@@ -1,35 +1,36 @@
-import {pluralize, el, text, tag} from "./utils";
+import "whatwg-fetch"; // global.fetch
+import prettyMs from "pretty-ms";
+
+import {pluralize, el, text, tag, checkStatus, parseJSON} from "./utils";
 
 const now = new Date();
 
-const checkStatus = (response) => {
-  if (response.status >= 200 && response.status < 300) {
-    return response;
-  }
-  const error = new Error(response.statusText);
-  error.response = response;
-  throw error;
-};
-const parseJSON = (response) => {
-  return response.json();
-};
-
 const compareStartDates = (a, b) => a.starts_on < b.starts_on ? -1 : a.starts_on == b.starts_on ? 0 : 1;
 
+const maxOf = metric => (max, data) => data[metric] > max ? data[metric] : max;
+
 function fetchToursData() {
-  // TODO this is still a bare `fetch` in the bundled js
-  fetch("//phish.in/api/v1/tours?per_page=100")
-    .then(checkStatus)
-    .then(parseJSON)
+  fetch("//phish.in/api/v1/tours?per_page=100").then(checkStatus).then(parseJSON)
     .then(({data: toursData}) => {
-      console.log("toursData", toursData);
       const toursDataSorted = toursData.sort(compareStartDates);
       document.body.appendChild(
         tag.div(
           tag.h1(`In Phish's ${pluralize(toursData.length, "tour")}...`),
-          tag.p(`The most shows they played in a "tour" was ${/*toursData.reduce(max("shows_count"))*/"?"}`)
+          tag.p(`The most shows they played in a tour was ${toursData.reduce(maxOf("shows_count"), 0)}.`)
         )
       );
+
+      const tourUrls = toursDataSorted.map(tourData => `//phish.in/api/v1/tours/${tourData.id}`);
+      Promise.all(tourUrls.map(tourUrl => fetch(tourUrl).then(checkStatus).then(parseJSON)))
+        .then(promiseData => {
+          const data = promiseData.map(d => d.data);
+          const durationOfEachTour = data.map(tourData => tourData.shows.reduce((total, showData) => showData.duration + total, 0));
+          document.body.appendChild(
+            tag.div(
+              tag.p(`Total duration played: ${prettyMs(durationOfEachTour.reduce((a, b) => a + b))}`)
+            )
+          );
+        });
     });
 }
 
--- src/utils.js
+++ src/utils.js
@@ -28,3 +28,15 @@ export const tag = {
 ["h1", "h2", "p"].forEach((tagName) => {
   tag[tagName] = (content) => el(tagName, text(content));
 });
+
+export const checkStatus = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+  const error = new Error(response.statusText);
+  error.response = response;
+  throw error;
+};
+export const parseJSON = (response) => {
+  return response.json();
+};
